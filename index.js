50f38999d282183ed88843d7fbc40626e58c5d2c
Fernanda-Falcão <nanda_oliveirafalcao@hotmail.com>
2025-04-01 00:40:27 -0300

    Codigo-finalizado

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Desafio2")
+function calcularNivel(vitorias, derrotas){
+    let saldo = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10){
+        nivel = "Ferro";
+    }else if (vitorias <= 20){
+        nivel = "Bronze";
+    }else if (vitorias<=50){
+        nivel = "Prata";
+    }else if (vitorias<= 80){
+        nivel = "Ouro";
+    }else if (vitorias <= 90){
+        nivel = "Diamante";
+    }else if (vitorias<= 100){
+        nivel = "Lendário";
+    }else {
+        nivel = "Imortal";
+    }
+    return `O Herói tem de saldo de ${saldo} e está no nível ${nivel}.`;
+}
+let vitorias = 85;
+let derrotas = 30;
+console.log(calcularNivel(vitorias, derrotas));
